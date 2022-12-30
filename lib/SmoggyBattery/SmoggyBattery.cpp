27d5b1964032be2ef3ca39c758cd35ca89a21064
stsdc <6031763+stsdc@users.noreply.github.com>
2022-12-30 18:15:19 +0100

    Add battery sampling

--- lib/SmoggyBattery/SmoggyBattery.cpp
+++ lib/SmoggyBattery/SmoggyBattery.cpp
@@ -1,17 +1,31 @@
 #include "SmoggyBattery.hpp"
 
-Battery::Battery() {}
+Battery::Battery() {
+}
 
 float Battery::get_vin() {
-  return (analogRead(PIN_ADC_BAT) * ADC_REFERENCE) / (4095);
+    int adcSum = 0;
+    int num_of_samples = 50;
+    for (int i = 0; i < num_of_samples; i++) {
+        adcSum += analogRead(PIN_ADC_BAT);
+        delay(50);
+    }
+
+    auto vin = ((adcSum/num_of_samples) * ADC_REFERENCE) / (4095);
+    Serial.println("⚡ Battery: Vin: " + String(vin));
+  return vin;
 }
 
 float Battery::get_vbat() {
-  return ((R1 + R2) * this->get_vin()) / R2;
+    auto vbat = ((R1 + R2) * this->get_vin()) / R2;
+    Serial.println("⚡ Battery: Vbat: " + String(vbat));
+  return vbat;
 }
 
 float Battery::get_percentage() {
-  return 100 * (this->get_vbat() - VOLTAGE_BAT_MIN) / (VOLTAGE_BAT_MAX - VOLTAGE_BAT_MIN);
+    auto percentage = 100 * (this->get_vbat() - VOLTAGE_BAT_MIN) / (VOLTAGE_BAT_MAX - VOLTAGE_BAT_MIN);
+    Serial.println("⚡ Battery: " + String(percentage)) + "%%";
+  return percentage;
 }
 
 void Battery::print() {
